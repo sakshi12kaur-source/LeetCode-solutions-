25b3f50498786e2a7ba89ecf5c1fd9ca601118d3
Sakshi12kaur <sakshi12kaur@gmail.com>
2026-07-08 21:46:37 +0530

    Time: 46 ms (43.85%) | Memory: 53.7 MB (51.12%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,13 @@
+var expect = function(val) {
+    return {
+        toBe: function(value) {
+            if (val === value) return true;
+            throw new Error("Not Equal");
+        },
+
+        notToBe: function(value) {
+            if (val !== value) return true;
+            throw new Error("Equal");
+        }
+    };
+};
