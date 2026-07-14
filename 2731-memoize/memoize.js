643e686e3e3eaa42c75b4ca34384c9400134a2b0
Sakshi12kaur <sakshi12kaur@gmail.com>
2026-07-14 16:50:33 +0530

    Time: 241 ms (90.40%) | Memory: 95 MB (59.52%) - LeetSync

---
+++ 2731-memoize/memoize.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var memoize = function(fn) {
+
+    const cache = new Map();
+
+    return function (...args) {
+
+        const key = JSON.stringify(args);
+
+        if (cache.has(key)) {
+            return cache.get(key);
+        }
+
+        const result = fn(...args);
+
+        cache.set(key, result);
+
+        return result;
+    }
+};
